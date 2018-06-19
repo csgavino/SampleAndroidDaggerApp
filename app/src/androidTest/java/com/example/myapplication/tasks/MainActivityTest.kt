484503566e4a2d21426813f058a4e596e4cdb705
Carlos Gavino <cs.gavino@gmail.com>
2018-06-19 15:35:07 +0900

    Use page object pattern in Espresso tests

--- app/src/androidTest/java/com/example/myapplication/tasks/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/myapplication/tasks/MainActivityTest.kt
@@ -1,16 +1,8 @@
 package com.example.myapplication.tasks
 
-import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.action.ViewActions.click
-import android.support.test.espresso.action.ViewActions.typeText
-import android.support.test.espresso.assertion.ViewAssertions.matches
-import android.support.test.espresso.matcher.ViewMatchers.withId
-import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.rule.ActivityTestRule
-import com.example.myapplication.R
 import org.junit.Rule
 import org.junit.Test
-
 
 class MainActivityTest {
 
@@ -19,8 +11,11 @@ class MainActivityTest {
 
     @Test
     fun tasks() {
-        onView(withId(R.id.edit_text_email)).perform(typeText("foo@example.com"))
-        onView(withId(R.id.btn_submit)).perform(click())
-        onView(withId(R.id.text_view_tasks)).check(matches(withText("Espresso")))
+        val mainPage = MainPage()
+
+        mainPage.email = "foo@example.com"
+        mainPage.submit()
+
+        mainPage.assertSuccess()
     }
 }
---
+++ app/src/androidTest/java/com/example/myapplication/tasks/MainPage.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication.tasks
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.ViewInteraction
+import android.support.test.espresso.action.ViewActions
+import android.support.test.espresso.assertion.ViewAssertions
+import android.support.test.espresso.matcher.ViewMatchers
+import com.example.myapplication.R
+
+class MainPage {
+
+    var email: String
+        get() = this.toString()
+        set(email) {
+            onView(ViewMatchers.withId(R.id.edit_text_email))
+                    .perform(ViewActions.typeText(email))
+        }
+
+    fun submit(): ViewInteraction =
+            onView(ViewMatchers.withId(R.id.btn_submit))
+                    .perform(ViewActions.click())
+
+    fun assertSuccess(): ViewInteraction =
+            onView(ViewMatchers.withId(R.id.text_view_tasks))
+                    .check(ViewAssertions.matches(ViewMatchers.withText("Espresso")))
+}
